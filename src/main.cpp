8715951aaf4fdff45c479524b446a6756a6481e1
Anjana Rajam <64962809+anjanarajam@users.noreply.github.com>
2020-09-12 12:08:35 +0200

    Fix compiler error

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ int main() {
   /* Initialize the steering and throttle pid variable */
   steer_pid.Init(0.25, 0.001, 3.0);
 
-  h.onMessage([&steer_pid, &throttle_pid, &twiddle](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&steer_pid, &twiddle](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
 					 uWS::OpCode opCode) {
 	// "42" at the start of the message means there's a websocket message event.
 	// The 4 signifies a websocket message
